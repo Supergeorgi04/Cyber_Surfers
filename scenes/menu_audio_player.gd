c8d9f0cd08d8627c7b27afd1a8b2e088e9695306
GeorgeLifo <georgevartalamis@gmail.com>
2026-06-10 20:11:08 +0300

    Final Build

--- scenes/menu_audio_player.gd
+++
@@ -1,8 +0,0 @@
-extends AudioStreamPlayer
-
-
-# Called when the node enters the scene tree for the first time.
-func play_music():
-	if playing:
-		return
-	play()
--- scripts/start_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-func _ready():
-	MenuAudioPlayer.play_music()
-
-func _on_pressed() -> void:
-	MenuAudioPlayer.stop()
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
-	pass # Replace with function body.
